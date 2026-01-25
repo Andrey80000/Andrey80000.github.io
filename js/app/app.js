ef05472595e52f9b4251e180ea57e449f41bea4a
Andrey <monstrtivender@gmail.com>
2026-01-25 16:43:28 +0200

    Add files via upload

--- js/app/app.js
+++ js/app/app.js
@@ -1,11 +1,13 @@
 import {router} from './router.js'
 import {msg} from './widgets/msg.js';
+import {popup} from './widgets/popup.js';
+import {header} from './widgets/header.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const main = {
         data() {
             return {
-                url:"https://affiliate.yanbasok.com",
+                url:"http://affiliate.yanbasok.com",
                 user: {name:"", phone:"", email:"", date:"", auth:""},
                 formData: {},
                 title: "",
@@ -71,6 +73,8 @@ document.addEventListener('DOMContentLoaded', function() {
     };
 
     var app = Vue.createApp(main)
+    .component('Header', header)
+    .component('popup', popup)
     .component('msg', msg)
     .use(router)
     .mount('#content')
